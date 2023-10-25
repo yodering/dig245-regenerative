c2780454719abb7e32867c02922ac4f7b6b947b1
David Yoder <david.yoder@live.com>
2023-10-25 16:21:48 -0400

    more styles and api key

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,27 @@
 
 /* javascript */
-const apiKey = "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"
+const apiKey = "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";
+const maxImages = 4;
+const imageUrls = [];
+
+
+
+const response = fetch(
+    "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5",
+    {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${apiKey}`,
+        },
+        body: JSON.stringify({
+            inputs: "cat"
+          })
+    }
+)
+
+if (!response.ok) {
+    alert("Failed to generate image!");
+}
+
+
