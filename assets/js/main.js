5df457cf65c9eb229ef63aef1a111a56f2ef3730
David Yoder <david.yoder@live.com>
2023-10-30 15:15:39 -0400

    added js for image gen
    cd
    ls

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,26 +2,60 @@
 /* javascript */
 const apiKey = "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";
 const maxImages = 4;
-const imageUrls = [];
+let imageNumber = null;
 
-
-
-const response = fetch(
-    "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5",
-    {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${apiKey}`,
-        },
-        body: JSON.stringify({
-            inputs: "cat"
-          })
-    }
-)
-
-if (!response.ok) {
-    alert("Failed to generate image!");
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+// Function to generate images
+async function generateImages(input) {
+
+    const loading = document.getElementById("loading");
+    loading.style.display = "block";
+
+    const imageUrls = [];
+
+    for (let i = 0; i < maxImages; i++) {
+        // generate a random number between 1 and 10000 and append it to the prompt
+        const randomNumber = randomNumber(1, 10000);
+        const prompt = `${input} ${randomNumber}`;
+        // add random number to prompt to create different results
+        const response = await fetch(
+            "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${apiKey}`,
+                },
+                body: JSON.stringify({ inputs: prompt }),
+            }
+        );
+
+        if (!response.ok) {
+            alert("Failed to generate image!");
+        }
+
+        const blob = await response.blob();
+        const imgUrl = URL.createObjectURL(blob);
+        imageUrls.push(imgUrl);
+
+        const img = document.createElement("img");
+        img.src = imgUrl;
+        img.alt = `art-${i + 1}`;
+        img.onclick = () => downloadImage(imgUrl, i);
+        document.getElementById("image-grid").appendChild(img);
+    }
+
+    loading.style.display = "none";
+
+    selectedImageNumber = null; // Reset selected image number
+}
+
+document.getElementById("generate").addEventListener('click', () => {
+    const input = document.getElementById("user-prompt").value;
+    generateImages(input);
+});
 
 
